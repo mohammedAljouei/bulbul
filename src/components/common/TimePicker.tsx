05c531c5622556561845f96e4697e610e071dfee
mohammedaljouei <mohammed.aljouei@gmail.com>
2025-02-08 18:41:17 +0300

    change time picker

--- src/components/common/TimePicker.tsx
+++ src/components/common/TimePicker.tsx
@@ -9,8 +9,8 @@ interface TimePickerProps {
 export function TimePicker({ value, onChange }: TimePickerProps) {
   // Available time slots
   const timeSlots = [
-    '09:00', '10:00', '11:00', '12:00', '13:00',
-    '14:00', '15:00', '16:00', '17:00', '18:00'
+    '14:00', '15:00', '16:00', '17:00', '18:00',
+    '19:00', '20:00', '21:00', '22:00', '23:00'
   ];
 
   const formatTime = (timeStr: string) => {
